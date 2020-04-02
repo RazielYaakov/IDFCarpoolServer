9fc5706d44d7121e714b86ea3a0fcc4de54bbf71
RazielYaakov <RazielYaakov@users.noreply.github.com>
2020-04-02 20:48:41 +0300

    Update RidesHandler.py

--- RidesHandler.py
+++ RidesHandler.py
@@ -233,10 +233,11 @@ def accept_ride(type_of_user, ride_id):
 def get_user_ride_requests(type_of_user, user_id):
     all_rides = get_all_rides_from_db()
     user_rides = []
-
-    for ride in all_rides:
-        if ride[values_position].get(type_of_user).get(phone_number) == user_id:
-            user_rides.append(ride)
+    
+    if all_rides is not None:
+        for ride in all_rides:
+            if ride[values_position].get(type_of_user).get(phone_number) == user_id:
+                user_rides.append(ride)
 
     return user_rides
 
